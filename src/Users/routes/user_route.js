c51496100c3af8f7ec48579a761c0baa20cdbb9c
unknown <manish.verma@uolo.com>
2024-10-21 23:52:52 +0530

    forgot Feature update

--- src/Users/routes/user_route.js
+++ src/Users/routes/user_route.js
@@ -4,7 +4,7 @@ const {authenticate} = require('../../../setups/middleware')
 const { user_register, login_user ,update_user,  update_password} = require('../controllers/users_controllers');
 const router = express.Router();
 
-router.post('/register', user_register);
+router.post('/signup', user_register);
 router.post('/login', login_user);
 router.put('/updateusers',authenticate,update_user);
 router.put('/updatepassword',authenticate,update_password);
